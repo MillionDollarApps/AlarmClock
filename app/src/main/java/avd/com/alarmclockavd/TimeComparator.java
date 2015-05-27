8d5c7e39197d6fde6afbbd202cfee9678be260bf
ownage4ownage <nitustefandragos@gmail.com>
2015-05-27 22:35:33 +0300

    only 1 thing to do!

---
+++ app/src/main/java/avd/com/alarmclockavd/TimeComparator.java
@@ -0,0 +1,15 @@
+package avd.com.alarmclockavd;
+
+import java.util.Comparator;
+
+public class TimeComparator implements Comparator<Alarm> {
+
+    @Override
+    public int compare(Alarm lhs, Alarm rhs) {
+        if (lhs.getTime() > rhs.getTime())
+            return 1;
+        else if (lhs.getTime() < rhs.getTime())
+            return -1;
+        return 0;
+    }
+}
