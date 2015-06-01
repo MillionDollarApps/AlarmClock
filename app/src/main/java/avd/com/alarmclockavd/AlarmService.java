20ca241f1d9317f5308071cae192719b19532a3c
ownage4ownage <nitustefandragos@gmail.com>
2015-06-01 12:48:20 +0300

    add Icon, change name left

---
+++ app/src/main/java/avd/com/alarmclockavd/AlarmService.java
@@ -0,0 +1,23 @@
+package avd.com.alarmclockavd;
+
+import android.app.Service;
+import android.content.Intent;
+import android.os.IBinder;
+
+public class AlarmService extends Service {
+
+    @Override
+    public int onStartCommand(Intent intent, int flags, int startId) {
+
+        return Service.START_NOT_STICKY;
+
+    }
+
+    @Override
+    public IBinder onBind(Intent intent) {
+
+        return null;
+    }
+
+
+}
